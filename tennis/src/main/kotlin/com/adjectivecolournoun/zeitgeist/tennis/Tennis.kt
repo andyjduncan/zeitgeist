7dac3e85637da44d4f0fb1d2dc752c4c638055ec
Andy Duncan <andy@adjectivecolournoun.com>
2017-05-21 00:25:01 +0100

    Finite state machine

--- tennis/src/main/kotlin/com/adjectivecolournoun/zeitgeist/tennis/Tennis.kt
+++ tennis/src/main/kotlin/com/adjectivecolournoun/zeitgeist/tennis/Tennis.kt
@@ -2,45 +2,36 @@ package com.adjectivecolournoun.zeitgeist.tennis
 
 class Tennis {
 
-    var serverPoints = 0
-    var receiverPoints = 0
+    var state: String = "love-love"
+
+    private val SERVER = "server"
+
+    private val RECEIVER = "receiver"
+
+    private val transitions = mapOf(
+            "love-love" to mapOf(SERVER to "15-love", RECEIVER to "love-15"),
+            "15-love" to mapOf(SERVER to "30-love"),
+            "love-15" to mapOf(RECEIVER to "love-30"),
+            "30-love" to mapOf(SERVER to "40-love"),
+            "love-30" to mapOf(RECEIVER to "love-40"),
+            "40-love" to mapOf(SERVER to "server wins", RECEIVER to "40-15"),
+            "40-15" to mapOf(SERVER to "server wins", RECEIVER to "40-30"),
+            "love-40" to mapOf(RECEIVER to "receiver wins"),
+            "40-30" to mapOf(RECEIVER to "deuce"),
+            "deuce" to mapOf(SERVER to "advantage server", RECEIVER to "advantage receiver"),
+            "advantage server" to mapOf(SERVER to "server wins", RECEIVER to "deuce"),
+            "advantage receiver" to mapOf(SERVER to "deuce", RECEIVER to "receiver wins")
+    )
 
     fun serverScores() {
-        serverPoints++
+        state = transitions[state]?.get(SERVER) ?: throw IllegalStateException(state)
     }
 
     fun receiverScores() {
-        receiverPoints++
-    }
-
-    private fun scoreFor(points: Int): String {
-        return when (points) {
-            0 -> "love"
-            1 -> "15"
-            2 -> "30"
-            3 -> "40"
-            else -> throw IllegalArgumentException()
-        }
+        state = transitions[state]?.get(RECEIVER) ?: throw IllegalStateException(state)
     }
 
     fun score(): String {
-        if (serverPoints == 3 && receiverPoints == 3) {
-            return "deuce"
-        } else if (serverPoints >= 4) {
-            if (serverPoints == receiverPoints) {
-                return "deuce"
-            } else if (serverPoints - receiverPoints > 1) {
-                return "server wins"
-            }
-            return "advantage server"
-        } else if (receiverPoints >= 4) {
-            if (serverPoints == receiverPoints) {
-                return "deuce"
-            } else if (receiverPoints - serverPoints > 1) {
-                return "receiver wins"
-            }
-            return "advantage receiver"
-        }
-        return scoreFor(serverPoints) + "-" + scoreFor(receiverPoints)
+        return state
     }
 }
